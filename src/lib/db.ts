2060c5c24509782002976873c41d2f1d9e15109a
Roushankumar Mishra <mishraroushan455@gmail.com>
2026-08-18 01:15:27 +0530

    fix: replaced raw password entry from docker-compose.yml and import it from `.env`

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,12 +1,18 @@
-import { PrismaClient } from "@prisma/client/extension";
+import { PrismaClient } from "../../generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
 
 declare global {
   var prisma: PrismaClient | undefined;
 }
 
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+const adapter = new PrismaPg(pool);
+
 const db =
   globalThis.prisma ||
   new PrismaClient({
+    adapter,
     log: ["query", "info", "warn", "error"],
   });
 
